c29795d2bacb943afb27a976a0174865cb3399d5
Simone Smith <simonecaplin@gmail.com>
2019-04-03 16:18:54 +0100

    Render JSON data on page

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -28,18 +28,7 @@ class App extends Component<{}, IAppState> {
     return (
       <div className={styles.app}>
         <header className={styles.header}>
-          <Table />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className={styles.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Table researchRecords={this.state.driveFileList} />
         </header>
       </div>
     );
