de48e8759414f9e5175c65cf3dda376f6aca9e31
Simone Smith <simonecaplin@gmail.com>
2019-04-04 10:45:44 +0100

    Display custom properties in Table component

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -12,12 +12,22 @@ interface ITableState {
 interface IRowProps {
   title: string;
   output: string;
+  customProperties: {
+    okr: string;
+    team: string;
+    quarter: string;
+    year: string;
+  };
 }
 
-const Row = ({ title, output }: IRowProps) => (
+const Row = ({ title, output, customProperties }: IRowProps) => (
   <tr>
     <td>{title}</td>
     <td>{output}</td>
+    <td>{customProperties.okr}</td>
+    <td>{customProperties.team}</td>
+    <td>{customProperties.quarter}</td>
+    <td>{customProperties.year}</td>
   </tr>
 );
 
@@ -30,11 +40,15 @@ class Table extends Component<ITableProps, {}> {
           <tr>
             <th>Title</th>
             <th>Output</th>
+            <th>OKR</th>
+            <th>Team</th>
+            <th>Quarter</th>
+            <th>Year</th>
           </tr>
         </thead>
         <tbody>
           {this.props.researchRecords ? this.props.researchRecords.map(row => (
-            <Row key={row.title} title={row.title} output={row.output} />
+            <Row key={row.title} title={row.title} output={row.output} customProperties={row.customProperties} />
           )) : null}
         </tbody>
       </table>
--- client/src/types/DriveModel.ts
+++ client/src/types/DriveModel.ts
@@ -2,6 +2,12 @@ export interface IDriveFile {
     id: string;
     title: string;
     output: string;
+    customProperties: {
+        okr: string;
+        team: string;
+        quarter: string;
+        year: string;
+    };
 }
 
 export type DriveFileList = Array<IDriveFile>;
