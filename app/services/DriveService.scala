01449c972a0b353ddb9898158ae4d3fc01800056
vanessa <32312712+vlbee@users.noreply.github.com>
2019-04-05 11:18:09 +0100

    Add getAllRecord data processing - extractRawFile andThen transformToResearchRecord

--- app/services/DriveService.scala
+++ app/services/DriveService.scala
@@ -1,21 +1,18 @@
 package services
 
 import java.io.FileInputStream
+import scala.collection.JavaConverters._
+import scala.util.Try
 
-import play.api.libs.json.{JsNumber, JsString, JsSuccess, Json => PlayJson}
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.drive.model.File
 import com.google.api.services.drive.{DriveScopes, Drive => DriveClient}
+
 import models.Output.{Document, Presentation, Spreadsheet, UnknownDriveOutput, UnsupportedDriveOutput, Video}
 import models.Quarter.{Q1, Q2, Q3, Q4, UnknownQuarter}
-import models.Status.Complete
 import models._
-
-import scala.collection.JavaConverters._
-import scala.util.Try
-
 
 object DriveService {
 
@@ -24,53 +21,81 @@ object DriveService {
   private val filePath = new FileInputStream(Config.serviceAccountKey)
 
   private val credential: GoogleCredential = GoogleCredential.fromStream(filePath)
-
   private val scopedCredential = credential.createScoped(List(DriveScopes.DRIVE_READONLY).asJava)
 
   lazy val driveClient: DriveClient = new DriveClient.Builder(httpTransport, jacksonFactory, scopedCredential)
     .build()
 
-  def extractRawFile(file: File): DriveFile = {
-    // The inline Try will handle nulls returned from Java library
+  def getAllRecords: List[ResearchRecord] = {
+    val request = driveClient.files().list()
+      .setCorpora("teamDrive")
+      .setIncludeTeamDriveItems(true)
+      .setSupportsTeamDrives(true)
+      .setFields("*")
+      .setTeamDriveId(Config.teamDriveId)
+
+    request.execute().getFiles.asScala.toList
+      .map(file =>
+        // transformToResearchRecord(extractRawFile(file)) // define functions with def
+        (extractRawFile andThen transformToResearchRecord)(file) // define functions with val
+    )
+  }
+
+  val extractRawFile = (fileStream: File) => {
     val customProps = Try {
-      file.getProperties.asScala.toMap
+      fileStream.getProperties.asScala.toMap
     }.toOption.getOrElse(Map.empty)
-    val thumbnailLink = Try {
-      file.getThumbnailLink
-    }.toOption.getOrElse("")
     val exportLinks = Try {
-      file.getExportLinks.asScala.toMap
+      fileStream.getExportLinks.asScala.toMap
     }.toOption.getOrElse(Map.empty)
 
     DriveFile(
-      id = file.getId,
-      title = file.getName,
-      output = file.getMimeType,
-      outputPreview = file.getWebViewLink, // TODO - process to preview
-      outputDownload = file.getWebContentLink, // TODO - returning nulls
-      outputThumbnail = thumbnailLink, // TODO - returning 404s
+      id = fileStream.getId,
+      title = fileStream.getName,
+      output = fileStream.getMimeType,
+      outputPreview = fileStream.getWebViewLink,
+      outputDownload = fileStream.getWebContentLink, // TODO - returning nulls
       exportLinks = exportLinks,
       customProperties = customProps
     )
   }
 
-  def convertToPreview(str: String) = {
+  val transformToResearchRecord = (file: DriveFile) => {
+    ResearchRecord.apply(
+      id = file.id,
+      title = file.title,
+      team = file.customProperties.getOrElse("team", "Unknown Team"),
+      output = setOutputType(file.output),
+      outputUrl = convertToPreviewLink(file.outputPreview),
+      outputThumbnail = createThumbnailLink(file.id),
+      outputDownload = file.outputDownload, // TODO returning null - permissions issue?
+      okr = file.customProperties.getOrElse("okr", ""),
+      year = file.customProperties.getOrElse("year", "0").toInt,
+      quarter = setQuarter(file.customProperties.getOrElse("quarter", "Unknown Quarter")),
+      tags = List("testtag") // Generate Random Tags
+    )
+  }
+
+  def convertToPreviewLink(str: String) = {
     str.replaceAll("edit", "preview")
   }
 
-
-  def getQuarter(str: String): Quarter = {
+  def setQuarter(str: String): Quarter = {
     str match {
+      case "1" => Q1
+      case "2" => Q2
+      case "3" => Q3
+      case "4" => Q4
       case "Q1" => Q1
       case "Q2" => Q2
       case "Q3" => Q3
       case "Q4" => Q4
-      case "Unknown" => UnknownQuarter
+      case "Unknown Quarter" => UnknownQuarter
       case _ => UnknownQuarter
     }
   }
 
-  def getOutputType(str: String): Output = {
+  def setOutputType(str: String): Output = {
     str match {
       case "application/vnd.google-apps.presentation" => Presentation
       case "application/vnd.google-apps.document" => Document
@@ -81,35 +106,10 @@ object DriveService {
     }
   }
 
-  def transformToResearchRecord(file: DriveFile): ResearchRecord = {
-
-    ResearchRecord.apply(
-      id = file.id,
-      title = file.title,
-      team = file.customProperties.getOrElse("team", ""),
-      output = getOutputType(file.output),
-      outputUrl = convertToPreview(file.outputPreview),
-      outputThumbnail = file.outputThumbnail,
-      okr = file.customProperties.getOrElse("okr", ""),
-      year = file.customProperties.getOrElse("year", "0").toInt,
-      quarter = getQuarter("quarter"),
-      tags = List("testtag")
-    )
+  def createThumbnailLink(id: String): String = {
+    s"https://drive.google.com/thumbnail?authuser=0&sz=w320&id=$id"
   }
 
-  def getAllRecords: List[ResearchRecord] = {
-    val request = driveClient.files().list()
-      .setCorpora("teamDrive")
-      .setIncludeTeamDriveItems(true)
-      .setSupportsTeamDrives(true)
-      .setFields("*")
-      .setTeamDriveId(Config.teamDriveId)
-
-      request.execute().getFiles.asScala.toList
-        .map(file =>
-          transformToResearchRecord(extractRawFile(file))
-        )
-  }
 }
 
 
