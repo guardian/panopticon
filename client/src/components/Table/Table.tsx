c8d2345042f5db8f4032a0d3af4364db93e6abb1
Simone Smith <simonecaplin@gmail.com>
2019-04-05 10:06:14 +0100

    Add more columns to table, more formatting

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -7,8 +7,15 @@ interface ITableProps {
 }
 
 interface IRowProps {
+  button: string;
   title: string;
   output: string;
+  outputPreview: string;
+  outputDownload: string;
+  outputThumbnail: string;
+  exportLinks: {
+    link: string
+  };
   customProperties: {
     okr: string;
     team: string;
@@ -17,10 +24,15 @@ interface IRowProps {
   };
 }
 
-const Row = ({ title, output, customProperties }: IRowProps) => (
+const Row = ({ title, output, outputPreview, outputDownload, outputThumbnail, exportLinks, customProperties }: IRowProps) => (
   <tr>
+    <td><button>+</button></td>
     <td>{title}</td>
     <td>{output}</td>
+    <td>{outputPreview}</td>
+    <td>{outputDownload}</td>
+    <td>{outputThumbnail}</td>
+    <td>{exportLinks.link}</td>
     <td>{customProperties.okr}</td>
     <td>{customProperties.team}</td>
     <td>{customProperties.quarter}</td>
@@ -32,23 +44,38 @@ class Table extends Component<ITableProps, {}> {
 
   render() {
     return (
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Output</th>
-            <th>OKR</th>
-            <th>Team</th>
-            <th>Quarter</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.researchRecords ? this.props.researchRecords.map(row => (
-            <Row key={row.title} title={row.title} output={row.output} customProperties={row.customProperties} />
-          )) : null}
-        </tbody>
-      </table>
+      <div className={styles.table}>
+        <table>
+          <thead>
+            <tr>
+              <th>Expand</th>
+              <th>Title</th>
+              <th>Output</th>
+              <th>Preview</th>
+              <th>Download</th>
+              <th>Thumbnail</th>
+              <th>Export Links</th>
+              <th>OKR</th>
+              <th>Team</th>
+              <th>Quarter</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.researchRecords ? this.props.researchRecords.map(row => (
+              <Row key={row.id}
+                button={row.button}
+                title={row.title}
+                output={row.output}
+                outputPreview={row.outputPreview}
+                outputDownload={row.outputDownload}
+                outputThumbnail={row.outputThumbnail}
+                exportLinks={row.exportLinks}
+                customProperties={row.customProperties} />
+            )) : null}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- client/src/types/DriveModel.ts
+++ client/src/types/DriveModel.ts
@@ -1,7 +1,14 @@
 export interface IDriveFile {
     id: string;
+    button: string;
     title: string;
     output: string;
+    outputPreview: string;
+    outputDownload: string;
+    outputThumbnail: string;
+    exportLinks: {
+        link: string
+    };
     customProperties: {
         okr: string;
         team: string;
