61a2fcbae57022d52124a9e770a66aa781909db5
Francesca Hammond <francesca.hammond@guardian.co.uk>
2019-04-08 17:20:21 +0100

    Add expand row state and onClick handler

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -6,17 +6,26 @@ interface ITableProps {
   researchRecords: DriveFileList
 }
 
+interface ITableState {
+  expandedRows: Array<String>
+}
+
 interface IRowProps {
+  id: string;
   title: string;
   output: string;
   okr: string;
   team: string;
   quarter: string;
   year: string;
+  expandRow: (id:string) => void;
+  className: string;
 }
 
-const Row = ({ title, output, okr, team, quarter, year }: IRowProps) => (
-  <div className={styles.row} onClick={expandRow}>
+const Row = ({ id, title, output, okr, team, quarter, year, expandRow, className }: IRowProps) => (
+  <div className={className} onClick={(event:React.MouseEvent<HTMLDivElement>) => {
+    expandRow(id)
+  }}>
     <div className={styles.cellTitle}>{title}</div>
     <div className={styles.cell}>{output}</div>
     <div className={styles.cell}>{okr}</div>
@@ -26,11 +35,23 @@ const Row = ({ title, output, okr, team, quarter, year }: IRowProps) => (
   </div>
 );
 
-const expandRow = (event) => {
+class Table extends Component<ITableProps, ITableState> {
+  constructor(props: ITableProps) {
+    super(props);
+    this.state = {
+      expandedRows: []
+    }
+  }
 
-}
-
-class Table extends Component<ITableProps, {}> {
+  expandRow = (id:string) => {
+    this.setState((prevState) => {
+        if(prevState.expandedRows.includes(id)) {
+           return {expandedRows: []}
+        } else {
+           return {expandedRows: [id]}
+      };
+    });
+  }
 
   render() {
     return (
@@ -46,13 +67,16 @@ class Table extends Component<ITableProps, {}> {
         {this.props.researchRecords
           ? this.props.researchRecords.map(row => (
               <Row
-                key={row.title}
+                key={row.id}
+                id={row.id}
                 title={row.title}
                 output={row.output}
                 okr={row.okr}
                 team={row.team}
                 quarter={row.quarter}
                 year={row.year}
+                expandRow={this.expandRow}
+                className={this.state.expandedRows.includes(row.id) ? styles.newRow : styles.row}
               />
             ))
           : null}
